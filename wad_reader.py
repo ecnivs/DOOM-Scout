1c1e34dab812a04d428b505599124ec9d8b7fb9e
Vince Swu <vinceswu88@gmail.com>
2024-05-22 16:15:38 +0530

    update

--- wad_reader.py
+++ wad_reader.py
@@ -4,7 +4,19 @@ class WADReader:
     def __init__(self, path):
         self.wad_file = open(path, 'rb')
         self.header = self.read_header()
-        print('\n', self.header)
+        self.directory = self.read_directory()
+
+    def read_directory(self):
+        directory = []
+        for i in range(self.header['lump_count']):
+            offset = self.header['init_offset'] + i * 16
+            lump_info = {
+                'lump_offset': self.read_4_bytes(offset),
+                'lump_size': self.read_4_bytes(offset + 4),
+                'lump_name': self.read_string(offset + 8, num_bytes=8)
+            }
+            directory.append(lump_info)
+        return directory
 
     def read_header(self):
         return {
@@ -13,10 +25,20 @@ class WADReader:
             'init_offset': self.read_4_bytes(offset=8)
         }
 
+    def read_1_byte(self, offset, byte_format='B'):
+        # B - unsigned char, b - signed char
+        return self.read_bytes(offset=offset, num_bytes=1, byte_format=byte_format)[0]
+
+    def read_2_bytes(self, offset, byte_format):
+        # H - uint16, h - int16
+        return self.read_bytes(offset=offset, num_bytes=1, byte_format=byte_format)[0]
+
     def read_4_bytes(self, offset, byte_format = 'i'):
+        # I - uint16, i - int16
         return self.read_bytes(offset=offset, num_bytes=4,byte_format=byte_format)[0]
 
     def read_string(self, offset, num_bytes):
+        # c - char
         return ''.join(b.decode('ascii') for b in self.read_bytes(offset, num_bytes, byte_format='c' * num_bytes) if ord(b) !=0).upper()
 
     def read_bytes(self, offset, num_bytes, byte_format):
