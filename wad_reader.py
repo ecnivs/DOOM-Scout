ad2401e6d56c35bdeba6e1488246f806b292647e
Vince Swu <vinceswu88@gmail.com>
2024-05-22 20:06:49 +0530

    update

--- wad_reader.py
+++ wad_reader.py
@@ -1,11 +1,27 @@
 import struct
 from pygame.math import Vector2 as vec2
+from data_types import *
+
 
 class WADReader:
-    def __init__(self, path):
-        self.wad_file = open(path, 'rb')
+    def __init__(self, wad_path):
+        self.wad_file = open(wad_path, 'rb')
         self.header = self.read_header()
         self.directory = self.read_directory()
+
+    def read_linedef(self, offset):
+        # 14 bytes = 2H x 7
+        read_2_bytes = self.read_2_bytes
+
+        linedef = Lindedef()
+        linedef.start_vertex_id = read_2_bytes(offset, byte_format='H')
+        linedef.end_vertex_id = read_2_bytes(offset + 2, byte_format='H')
+        linedef.flags = read_2_bytes(offset + 4, byte_format='H')
+        linedef.line_type = read_2_bytes(offset + 6, byte_format='H')
+        linedef.sector_tag = read_2_bytes(offset + 8, byte_format='H')
+        linedef.front_sidedef_id = read_2_bytes(offset + 10, byte_format='H')
+        linedef.back_sidedef_id = read_2_bytes(offset + 12, byte_format='H')
+        return linedef
 
     def read_vertex(self, offset):
         # 4 bytes = 2h + 2h
@@ -38,15 +54,17 @@ class WADReader:
 
     def read_2_bytes(self, offset, byte_format):
         # H - uint16, h - int16
-        return self.read_bytes(offset=offset, num_bytes=1, byte_format=byte_format)[0]
+        return self.read_bytes(offset=offset, num_bytes=2, byte_format=byte_format)[0]
 
-    def read_4_bytes(self, offset, byte_format = 'i'):
-        # I - uint16, i - int16
-        return self.read_bytes(offset=offset, num_bytes=4,byte_format=byte_format)[0]
+    def read_4_bytes(self, offset, byte_format='i'):
+        # I - uint32, i - int32
+        return self.read_bytes(offset=offset, num_bytes=4, byte_format=byte_format)[0]
 
     def read_string(self, offset, num_bytes):
         # c - char
-        return ''.join(b.decode('ascii') for b in self.read_bytes(offset, num_bytes, byte_format='c' * num_bytes) if ord(b) !=0).upper()
+        return ''.join(b.decode('ascii') for b in
+                       self.read_bytes(offset, num_bytes, byte_format='c' * num_bytes)
+                       if ord(b) != 0).upper()
 
     def read_bytes(self, offset, num_bytes, byte_format):
         self.wad_file.seek(offset)
